cf632b00b095534efaa261c5dffebace89f6897d
BSoldi <briansoldani@gmail.com>
2020-10-03 11:58:16 -0700

    rrremove node modules

--- client/src/pages/Booking.js
+++ client/src/pages/Booking.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Grid, Cell } from 'react-mdl';
 import { GoogleLogin } from 'react-google-login';
-import API from "../utils/API"
-/* import jwt from 'jsonwebtoken'; */
+import API from "../utils/API";
+import BookingForm from "../components/BookingForm";
+
 
 const clientId = "205158470591-frud5g1h9dmquka6n1e3mhju7rglm33i.apps.googleusercontent.com";
 
@@ -12,11 +13,11 @@ const Booking = (props) => {
     const { setUser } = props;
     API.getUser(res.profileObj)
     //we can use this to get user info from saved token, so they don't have to log in again. -store in browser? local storage or session storage
-    // fetch("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + res.tokenId).then(res => res.json()).then(res => {
+    fetch("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + res.tokenId).then(res => res.json()).then(res => {
     //   //make a database req to check if user exists; if exists=> signin, if not, find a way to add user to db.
-    //   API.getUser(res)
-    //   setUser(res);
-    // });
+      API.getUser(res)
+      setUser(res);
+    });
   }
 
   const onAuthFailure = (res) => {
@@ -29,26 +30,7 @@ const Booking = (props) => {
         <Cell col={12}>
           {
             props.user === null ? (
-              <>
-                <br></br>
-
-                {/* <div>
-                  <Textfield
-                    onChange={() => { }}
-                    label="USERNAME"
-                    floatingLabel
-                  />
-                  <br></br>
-                  
-                  <Textfield
-                    onChange={() => { }}
-                    pattern="-?[0-9]*(\.[0-9]+)?"
-                    error="Input is not a number!"
-                    label="PASSWORD"
-                    floatingLabel
-                  />
-                  <Button type="submit" bsStyle="primary">Sign-In</Button>
-                </div> */}
+              <>               
                 <GoogleLogin
                   clientId={clientId}
                   //append client ID to user 
@@ -61,7 +43,8 @@ const Booking = (props) => {
             ) : (
                 <>
                   <h1>Welcome {props.user.name}</h1>
-                  <h2>Logged in as: {props.user.email}</h2>
+                  {/* <h2>Logged in as: {props.user.email}</h2> */}
+                  <BookingForm />
                 </>
               )
           }
