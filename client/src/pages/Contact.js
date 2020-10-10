88c9bba59a4cb5fdaf31c7869b256ba5854aa5ba
Terri J <terbr@hotmail.com>
2020-10-10 13:44:14 -0700

    working contact us page email

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -6,13 +6,13 @@ import axios from "axios";
 
 class Contact extends Component {
     constructor(props) {
-      super(props);
+      super();
       this.state = {
         name: '',
         email: '',
         message: ''
       }
-    
+    }
 
     handleSubmit(e) {
       e.preventDefault();
@@ -21,7 +21,7 @@ class Contact extends Component {
         method: "POST", 
         url:"http://localhost:3002/send", 
         data:  this.state
-      }).then((response)=>{
+      }).then(function(response){
         if (response.data.status === 'success') {
           alert("Message Sent."); 
           console.log("Message data", this.state);
@@ -44,14 +44,14 @@ class Contact extends Component {
             <img
               src="https://st.depositphotos.com/1636803/2416/i/950/depositphotos_24169067-stock-photo-sign-a-green-cross.jpg"
               alt="Big Green Cross"
-              style={{ height: '250px' }}
+              style={{ height: '150px' }}
             />
             <img
               src="https://www.cardfool.com/cards/assets/Big%20Wine%20Glass%20Val_cover.jpg"
               alt="Big Wine Glass"
-              style={{ height: '250px' }}
+              style={{ height: '150px' }}
             />
-            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>We specialize in wine and dispensery tours. Call today to reserve your tour!</p>
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>We specialize in wine and dispensery tours. Book or contact us today to reserve your tour!</p>
             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Flying solo? No problem. Check out our message board to connect with others looking to book tours in your area. No one wants to fly alone!</p>
 
           </Cell>
@@ -62,28 +62,28 @@ class Contact extends Component {
             <div className="contact-list">
               <List>
                 <ListItem>
-                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                  <ListItemContent style={{ fontSize: '20px', fontFamily: 'Anton' }}>
                     <i className="fa fa-phone-square" aria-hidden="true" />
                     (971) 655-5646
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                  <ListItemContent style={{ fontSize: '20px', fontFamily: 'Anton' }}>
                     <i className="fa fa-fax" aria-hidden="true" />
                     (503) 202-0401
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                  <ListItemContent style={{ fontSize: '20px', fontFamily: 'Anton' }}>
                     <i className="fa fa-envelope" aria-hidden="true" />
-                    RedGoldAndGreens@gmail.com
+                    RGGTours@gmail.com
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                  <ListItemContent style={{ fontSize: '20px', fontFamily: 'Anton' }}>
                     <i className="fa fa-skype" aria-hidden="true" />
                     RedGoldAndGreens@Skype
                   </ListItemContent>
@@ -93,20 +93,23 @@ class Contact extends Component {
               </List>
             </div>
           </Cell>
-          <Cell col={4} id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+          <Cell>
+            <form col={4} id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
           <div className="form-group">
-              <label htmlFor="name">Name</label>
+              <label htmlFor="name" style={{ fontSize: '20px', fontFamily: 'Anton' }}>Name</label>
               <input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
           </div>
           <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
+              <label htmlFor="exampleInputEmail1" style={{ fontSize: '20px', fontFamily: 'Anton' }}>Email address</label>
               <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
           </div>
           <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+              <label htmlFor="message" style={{ fontSize: '20px', fontFamily: 'Anton' }}>Message</label>
+              <div>
+              <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} /> </div>
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
           </Cell>
         </Grid>
       </div>
@@ -125,6 +128,7 @@ class Contact extends Component {
     this.setState({message: event.target.value})
   }
 
+  
 }
 
 export default Contact;
