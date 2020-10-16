e717aecfcda1cf64ef19c3a5eb0020ed57eda20b
Mohamed Abdi <lovegoldone@gmail>
2020-10-15 20:34:46 -0700

    better me

--- client/src/pages/Landingpage.js
+++ client/src/pages/Landingpage.js
@@ -1,4 +1,4 @@
-import React, { Component, Linking } from 'react';
+import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 
 import SocialLinks from "../components/SocialLinks";
@@ -27,7 +27,7 @@ class Landingpage extends Component {
               <hr />
 
               <p>RGG Tours is your premium tour operator in Oregon.<br></br> We specialize in <span className="span-winery">Winery</span>, <span className="span-brewery">Brewery</span>, and <span className="span-dispensary">Dispensary</span> tours throughout the state.</p>
-              <p><span className="span-book"><a href="http://localhost:3000/booking">Book Now!</a></span>  -- Find out more about our tours <span className="span-here"> <a href="http://localhost:3000/about">here!</a></span>.</p>
+              <p><span className="span-book"><a href="https://reds-gold-greens.herokuapp.com/booking">Book Now!</a></span>  -- Find out more about our tours <span className="span-here"> <a href="https://reds-gold-greens.herokuapp.com/about">here!</a></span>.</p>
               <p>We're here to make sure you have a great time!</p>
 
 
