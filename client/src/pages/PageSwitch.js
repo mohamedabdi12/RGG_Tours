9e66a7e9c2dfac04d39debc283f7ef2499b701f9
Max Edmonds <maxedmonds05@gmail.com>
2020-10-16 12:51:54 -0700

    add code to preload images

--- client/src/pages/PageSwitch.js
+++ client/src/pages/PageSwitch.js
@@ -11,6 +11,7 @@ import Booking from './Booking';
 
 const PageSwitch = () => {
   const [user, setUser] = useState(null);
+  const [tourImages, setTourImages] = useState({})
   useEffect(() => {
     API.currentUser().then(res => {
       setUser(res.data);
@@ -18,13 +19,32 @@ const PageSwitch = () => {
 
     })
   }, [])
+
+  //array of promises, passing into promise.all; once finished, the axios array of responses is converted into an array of arrays
+  useEffect(() => {
+    API.getTours().then(async ({ data: tours }) => {
+      const tourImages = await Promise.all(tours.map(({ id, bgImg }) => {
+        const image = new Image();
+        image.src = bgImg;
+        return Promise.all([id, image.decode().then(() => image)]);
+      }));
+      setTourImages(() => tourImages.reduce((map, [id, img]) => {
+        map[id] = img;
+        return map;
+      }, {})
+      );
+    });
+  }, []);
+
   return (<Switch>
     <Route exact path="/">
       <LandingPage user={user} setUser={setUser} />
     </Route>
     <Route path="/about" component={About} />
     <Route path="/contact" component={Contact} />
-    <Route path="/tours" component={Tours} />
+    <Route path="/tours">
+      <Tours />
+    </Route>
     <Route exact path="/booking">
       <Booking user={user} setUser={setUser} />
     </Route>
